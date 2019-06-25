6b2d68881332b7c9a6717504d9ec8db37ba79d06
Diana <kpds7175@gmail.com>
2019-06-25 19:45:12 +0300

    lombok added

---
+++ src/main/java/hillel/spring/Apple.java
@@ -0,0 +1,11 @@
+package hillel.spring;
+
+import java.util.Objects;
+
+import lombok.Data;
+
+@Data
+public class Apple {
+    private final Integer weight;
+    private final String color;
+}
--- src/test/java/hillel/spring/AppleTest.java
+++ src/test/java/hillel/spring/AppleTest.java
@@ -1,14 +1,19 @@
 package hillel.spring;
 
 import org.junit.Test;
+import lombok.val;
+import org.assertj.core.api.Assertions;
 
 import static org.junit.Assert.*;
 
 public class AppleTest {
-    @Test
-    public void lombokWorks()throws Exception{
+        @Test
+        public void lombokWorks() throws Exception {
+                val apple = new Apple(100, "Green");
 
+               Assertions. assertThat(apple.getColor()).isEqualTo("Green");
 
-    }
+               Assertions. assertThat(apple).isEqualTo(new Apple(100, "Green"));
 
+        }
 }
